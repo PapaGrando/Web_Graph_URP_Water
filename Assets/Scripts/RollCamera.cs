3bb2dbb3212dc37f0f957b6f5461775709be4fb5
PapaGrando <borislovesalo@gmail.com>
2020-09-12 14:25:43 +0300

    фикс воды

---
+++ Assets/Scripts/RollCamera.cs
@@ -0,0 +1,12 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class RollCamera : MonoBehaviour
+{
+    // Update is called once per frame
+    void Update()
+    {
+        transform.Rotate(new Vector3(-5f,0,0) * Time.deltaTime);
+    }
+}
