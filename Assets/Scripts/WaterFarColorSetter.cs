be2f946bec37d8680e30ec0ce4843818b1577a4d
PapaGrando <borislovesalo@gmail.com>
2020-09-22 19:31:18 +0300

    Плоскость с водой издалека

---
+++ Assets/Scripts/WaterFarColorSetter.cs
@@ -0,0 +1,26 @@
+﻿using UnityEngine;
+
+[ExecuteInEditMode]
+public class WaterFarColorSetter : MonoBehaviour
+{
+#pragma warning disable CS0649
+    [SerializeField] private Material _waterActiveMaterial;
+    private Material _currentMaterial;
+#pragma warning restore CS0649
+
+    void Awake()
+    {
+        _currentMaterial = GetComponent<Renderer>().sharedMaterial;
+
+        if (_currentMaterial == null && _waterActiveMaterial == null)
+        {
+            enabled = false;
+            Debug.LogWarning($"Some Materials in {gameObject.name} is null, color cant be set");
+        }
+    }
+
+    void Update()
+    {
+        _currentMaterial.color = _waterActiveMaterial.GetColor("Color_A13A931D"); //Water Far
+    }
+}
