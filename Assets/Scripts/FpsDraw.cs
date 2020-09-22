faa630e37a08f9b83ae8f1f493d76c398f718e8f
PapaGrando <borislovesalo@gmail.com>
2020-09-22 19:30:31 +0300

    Отображение фпс

---
+++ Assets/Scripts/FpsDraw.cs
@@ -0,0 +1,20 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+
+public class FpsDraw : MonoBehaviour
+{
+    public Text _fpsText;
+    public float _deltaTime;
+
+    private void Awake()
+    {
+        _fpsText = GetComponent<Text>();
+
+    }
+    void Update()
+    {
+        _deltaTime += (Time.deltaTime - _deltaTime) * 0.1f;
+        float fps = 1.0f / _deltaTime;
+        _fpsText.text = Mathf.Ceil(fps).ToString() + "FPS";
+    }
+}
